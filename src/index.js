ee7aeba72dbdf82666626594ec84f419f8ccf266
Dzyubinskiy Aleksandr <timik_333@mail.ru>
2021-02-25 22:37:20 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+    let minimum = array[0];
+    for (let i = 0; i < array.length; i++) {
+        let temp = array[i];
+        if (minimum < temp) {
+            minimum = minimum
+        } else {
+minimum = temp
+        }
+    }
+    if (minimum === undefined) {return 0} else {
+  return minimum;
+}
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+    let maximum = array[0]
+    for (let i = 0; i < array.length; i++) {
+        let temp = array[i];
+        if (maximum > temp) {
+            maximum = maximum
+        }
+        else {
+            maximum = temp
+        }
+    }
+    if (maximum === undefined) {return 0} else {
+  return maximum;
+}
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+    let newVar = 0;
+    for (let i = 0; i < array.length; i++) {
+        newVar += array[i]/array.length;
+    }
+  return newVar.toFixed(2);
 }
